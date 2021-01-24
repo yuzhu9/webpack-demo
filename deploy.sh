679336a9002427511f3351896f5cea116cbe6973
yuzhu9 <poly2014w@163.com>
2021-01-25 07:08:17 +0800

    remove disploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-yarn build &&
-git checkout gh-pages &&
-rm -rf src *.sh *.js *.json *.html *.png *.jpg *.svg &&
-mv dist/* ./ &&
-rm -rf dist;
-git add . &&
-git commit -m 'update' &&
-git push &&
-git checkout - 
